ce89f0f76681f6335d65b7b3397262a97b499d58
Daniel Costa <47618723+DanielCosta1232@users.noreply.github.com>
2019-04-01 18:52:04 -0400

    Readme fix

--- All_in_one_signin_script.ps1
+++ All_in_one_signin_script.ps1
@@ -2,7 +2,7 @@
 Note:
 The text prefix "cc" is added to all Security & Compliance Center cmdlet names so you can run cmdlets that exist in both Exchange Online and the Security & Compliance Center in the same Windows PowerShell session. For example, Get-RoleGroup becomes Get-ccRoleGroupin the Security & Compliance Center.
 
-NOTE:
+Using an MFA enabled account:
 If you run into errors when importing the Security & Compliance PowerShell session, you may need to install the Exchange PowerShell module that supports MFA authentications and use "Connect-IPPSession".
 Reference link:
 https://docs.microsoft.com/en-us/powershell/exchange/office-365-scc/connect-to-scc-powershell/mfa-connect-to-scc-powershell?view=exchange-ps
